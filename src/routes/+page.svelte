dfa809badc93cccff56b53049675582df44f4779
96kudye <yuudai.kuroki@link-u.co.jp>
2022-12-22 03:11:31 +0900

    article

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
     <h1>ブログ</h1>
     <ul>
       {#each data.contents as content}
-        <li><a href="{content.id}">{content.title}</a></li>
+        <li><a href="/article/{content.id}">{content.title}</a></li>
       {/each}
     </ul>
   </section>
