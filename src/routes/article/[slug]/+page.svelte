58e8e1921ec9da4ac99228ad7dd45829ee8ec43e
96kudye <yuudai.kuroki@link-u.co.jp>
2022-12-24 04:05:55 +0900

    tags on article

--- src/routes/article/[slug]/+page.svelte
+++ src/routes/article/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { PageData } from './$types';
   import { formatTime } from '$lib/dayjs';
+  import Button, { Label } from '@smui/button';
   export let data: PageData;
   let html = data.content
     .replaceAll('<blockquote>', '<blockquote><p>')
@@ -14,7 +15,18 @@
 
 <section>
   <h1 class="title">{data.title}</h1>
-  <h4>Published at {formatTime(data.publishedAt)}</h4>
+  <h4>
+    {#if data.tags}
+      Tags:
+      {#each data.tags as tag}
+        <a href="/tag/{tag.id}" class="link_tag">
+          <Button><Label>{tag.name}</Label></Button>
+        </a>
+      {/each}
+    {/if}
+    <br />
+    Published at {formatTime(data.publishedAt)}
+  </h4>
   <div class="article_container">
     {@html html}
   </div>
