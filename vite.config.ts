d8cfbbe6176a82072bbc8bc12440d18825220d0a
96kudye <yuudai.kuroki@link-u.co.jp>
2023-01-02 21:50:19 +0900

    reduce highlight.js, manualChunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
-const hljs_used_langs = [
-  'html',
-  'javascript'
-]
-
 const config: UserConfig = {
   plugins: [sveltekit()],
   build: {
@@ -20,16 +15,10 @@ const config: UserConfig = {
             return 'cheerio'
           }
           if (moduleName.includes('highlight')) {
-            if (moduleName.includes('languages')) {
-              return hljs_used_langs.some((lang) => moduleName.includes(lang)) ? 'highlight' : 'highlight_unused'
-            }
             return 'highlight'
           }
           if (moduleName.includes('node_modules')) {
             return 'vendor'
-          }
-          if (moduleName.includes('lib/components')) {
-            return 'components'
           }
           return 'main'
         }
