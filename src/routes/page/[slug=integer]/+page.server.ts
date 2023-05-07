3bd2a4988b8cf329ab9b3ce5314c9763d87ddaaa
96kudye <yuudai.kuroki@link-u.co.jp>
2023-05-07 23:46:06 +0900

    remove preparing pagenation

--- src/routes/page/[slug=integer]/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import { getArticleByUnixtime } from "$lib/microcms";
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = async ({ params }) => {
-  return await getArticleByUnixtime(parseInt(params.slug));
-};
