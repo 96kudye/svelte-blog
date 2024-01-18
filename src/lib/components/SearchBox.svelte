c1a0553ef1bf31446ba13450b12ae3b12489a146
96kudye <yuudai.kuroki@link-u.co.jp>
2024-01-18 21:22:35 +0900

    add dom content loaded log

--- src/lib/components/SearchBox.svelte
+++ src/lib/components/SearchBox.svelte
@@ -3,6 +3,7 @@
   <script src="/pagefind/pagefind-ui.js"></script>
   <script>
     window.addEventListener('DOMContentLoaded', (event) => {
+      console.log('DOM fully loaded and parsed');
       new PagefindUI({ element: '#search' });
     });
   </script>
