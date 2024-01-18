b4874d3b4b5c7697b0340240a4e22d156c5ea2cf
96kudye <yuudai.kuroki@link-u.co.jp>
2024-01-18 21:32:21 +0900

    event or just run

--- src/lib/components/SearchBox.svelte
+++ src/lib/components/SearchBox.svelte
@@ -2,10 +2,15 @@
   <link href="/pagefind/pagefind-ui.css" rel="stylesheet" />
   <script src="/pagefind/pagefind-ui.js"></script>
   <script>
-    window.addEventListener('DOMContentLoaded', (event) => {
-      console.log('DOM fully loaded and parsed');
+    if (document.readyState !== 'loading') {
+      console.log('document is already ready, just execute code here');
       new PagefindUI({ element: '#search' });
-    });
+    } else {
+      document.addEventListener('DOMContentLoaded', function () {
+        console.log('document was not ready, place code here');
+        new PagefindUI({ element: '#search' });
+      });
+    }
   </script>
 </svelte:head>
 
