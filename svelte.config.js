fa047085b0f8d2580804863187631b051d95e4e3
96kudye <yuudai.kuroki@link-u.co.jp>
2024-01-28 22:30:41 +0900

    revert static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,8 +9,7 @@ const config = {
 
   kit: {
     adapter: adapter({
-      trailingSlash: 'never',
-      fallback: 'index.html'
+      trailingSlash: 'never'
     })
   },
 };
